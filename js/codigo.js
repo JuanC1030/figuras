b95af995876654669a0f187cc3a2fd67082b4b44
JuanC1030 <73892428+JuanC1030@users.noreply.github.com>
2021-03-18 18:26:21 -0500

    Update and rename codigo.js to main.js

--- js/codigo.js
+++
@@ -1,7 +0,0 @@
-/*var usu = prompt("¿Cual es tu nombre?");
-var a = parseInt(prompt("Ingrese un número"));
-var b = parseInt(prompt("Ingrese otro número"));
-
-document.getElementById("textico").innerHTML = "Bienvenido "+usu+"<br><br> La suma de "+a+" más "+b+" es: "+(a+b);
-
-*/
---
+++ js/main.js
@@ -0,0 +1,47 @@
+function circuloFunction(){
+    var selector = document.getElementById("figura");
+    selector.className= '';
+    selector.classList.toggle("circulo");
+}
+
+function romboFunction(){
+    var selector = document.getElementById("figura");
+    selector.classList.toggle("rombo");
+    
+}
+function rectanguloFunction(){
+    var selector = document.getElementById("figura");
+    selector.classList.toggle("rectangulo");
+}
+function topFunction(){
+    var selector = document.getElementById("figura");
+    selector.classList.toggle("top");
+}
+function topFunction(){
+    var selector = document.getElementById("figura");
+    selector.classList.toggle("top");
+}
+function topFunction(){
+    var selector = document.getElementById("figura");
+    selector.classList.toggle("top");
+}
+function bottomFunction(){
+    var selector = document.getElementById("figura");
+    selector.classList.toggle("bottom");
+}
+function rigthFunction(){
+    var selector = document.getElementById("figura");
+    selector.classList.toggle("rigth");
+}
+function leftFunction(){
+    var selector = document.getElementById("figura");
+    selector.classList.toggle("left");
+}
+function diagonal1Function(){
+    var selector = document.getElementById("figura");
+    selector.classList.toggle("diagonal1");
+}
+function diagonal2Function(){
+    var selector = document.getElementById("figura");
+    selector.classList.toggle("diagonal2");
+}
